42e1cc60d3598771e9d8b67856437207d6c8b158
Sebastian <sebastianvr92@hotmail.com>
2021-03-23 22:51:35 -0500

    upgrade wgsi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,13 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
-import sys
-
-path = os.path.expanduser('~/mysite')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
