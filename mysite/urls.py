d8aeeb5e9fb00904667b6b301605ad88c3eda3fe
Sebastian <sebastianvr92@hotmail.com>
2021-04-22 20:41:07 -0500

    Upgrade mysite

--- mysite/urls.py
+++ mysite/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
         {'document_root': SITE_ROOT, 'show_indexes': True},
         name='site_path'
     ),
-    #url(r'^oauth/', include('social_django.urls', namespace='social')), 
+    url(r'^oauth/', include('social_django.urls', namespace='social')), 
     path('', TemplateView.as_view(template_name='home/main.html')),
     path('cookie', views.cookie),
     path('hello/', views.sessfun),
@@ -52,11 +52,13 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('autos/', include('autos.urls')),   
     path('cats/', include('cats.urls')),
+    path('', include('ads.urls')),
 
 ]
 
+
 # Serve the favicon - Keep for later
-'''urlpatterns += [
+urlpatterns += [
     path('favicon.ico', serve, {
             'path': 'favicon.ico',
             'document_root': os.path.join(BASE_DIR, 'home/static'),
@@ -66,12 +68,12 @@ urlpatterns = [
 
 # Switch to social login if it is configured - Keep for later
 try:
-    from mysite import github_settings
+    from home import github_settings
     social_login = 'registration/login_social.html'
     urlpatterns.insert(0,
                        path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
                        )
     print('Using', social_login, 'as the login template')
 except:
-    print('Using registration/login.html as the login template')'''
+    print('Using registration/login.html as the login template')
 
