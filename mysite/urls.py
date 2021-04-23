4874c39c20701486836c175c525d6e57375dbb8d
Sebastian <sebastianvr92@hotmail.com>
2021-04-22 21:28:00 -0500

    Upgrade mysite

--- mysite/urls.py
+++ mysite/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('autos/', include('autos.urls')),   
     path('cats/', include('cats.urls')),
-    path('', include('ads.urls')),
+    path('ads/', include('ads.urls')),
 
 ]
 
