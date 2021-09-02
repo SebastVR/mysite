3b00ac880dc03cd0ccbd05287dcfd24cb2d7ec4d
SebastVR <sebastianvr92@hotmail.com>
2021-09-01 21:41:37 -0500

    Added models mysite/urls.py, snippets/views.py and snippets/urls.py #8

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,7 @@ from django.views.static import serve
 from rest_framework import routers
 from quickstart import views
 
+
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
@@ -36,6 +37,7 @@ urlpatterns = [
     path("ads/", include("ads.urls", namespace="ads")),
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("", include("snippets.urls")),
 ]
 # Keep
 # Sample applications
