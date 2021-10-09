a71d7ab525203ec9afe3622984fac340cfd4082e
SebastVR <sebastianvr92@hotmail.com>
2021-10-09 10:47:52 -0500

    Add app tools #9

---
+++ tools/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from formtools.wizard.views import SessionWizardView
+
+
+class ContactWizard(SessionWizardView):
+    def done(self, form_list, **kwargs):
+        return render(
+            self.request,
+            "done.html",
+            {
+                "form_data": [form.cleaned_data for form in form_list],
+            },
+        )
